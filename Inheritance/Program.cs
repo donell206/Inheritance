9fbce330f2dc6022821b187828eaace9e643d580
Donnel <donnel206@hotmail.com>
2020-11-19 10:29:58 +0100

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class A
+    {
+        public string Name { get; set; }
+        public void Show1() 
+        {
+            Console.WriteLine("I am Parent method show1");
+        }
+    }
+
+    public class B : A
+    { 
+    
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B c = new B();
+            c.Show1();
+            c.Name = "Kenan";
+        
+        }
+    }
+}
